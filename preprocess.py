7d328fa3eefd7e4b7c51b166925e5976f20dda11
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-05 21:32:37 +0100

    preparing training and test datasets (including data augmentation)

---
+++ preprocess.py
@@ -0,0 +1,95 @@
+import os
+from glob import glob
+import shutil
+from tqdm import tqdm
+import dicom2nifti
+import numpy as np
+import nibabel as nib
+from monai.transforms import(
+    Compose,
+    AddChanneld,
+    LoadImaged,
+    Resized,
+    ToTensord,
+    Spacingd,
+    Orientationd,
+    ScaleIntensityRanged,
+    CropForegroundd,
+)
+from monai.data import DataLoader, Dataset, CacheDataset
+from monai.utils import set_determinism
+
+
+"""
+This file is for preporcessing only, it contains all the functions that you need
+to make your data ready for training.
+You need to install the required libraries if you do not already have them.
+pip install os, ...
+"""
+
+
+def prepare(in_dir, pixdim=(1.5, 1.5, 1.0), a_min=-200, a_max=200, spatial_size=[128,128,64], cache=False):
+
+    """
+    This function is for preprocessing, it contains only the basic transforms, but you can add more operations that you 
+    find in the Monai documentation.
+    https://monai.io/docs.html
+    """
+
+    set_determinism(seed=0)
+
+    path_train_volumes = sorted(glob(os.path.join(in_dir, "TrainVolumes", "*.nii.gz")))
+    path_train_segmentation = sorted(glob(os.path.join(in_dir, "TrainSegmentation", "*.nii.gz")))
+
+    path_test_volumes = sorted(glob(os.path.join(in_dir, "TestVolumes", "*.nii.gz")))
+    path_test_segmentation = sorted(glob(os.path.join(in_dir, "TestSegmentation", "*.nii.gz")))
+
+    train_files = [{"vol": image_name, "seg": label_name} for image_name, label_name in zip(path_train_volumes, path_train_segmentation)]
+    test_files = [{"vol": image_name, "seg": label_name} for image_name, label_name in zip(path_test_volumes, path_test_segmentation)]
+
+    train_transforms = Compose(
+        [
+            LoadImaged(keys=["vol", "seg"]),
+            AddChanneld(keys=["vol", "seg"]),
+            Spacingd(keys=["vol", "seg"], pixdim=pixdim, mode=("bilinear", "nearest")),
+            Orientationd(keys=["vol", "seg"], axcodes="RAS"),
+            ScaleIntensityRanged(keys=["vol"], a_min=a_min, a_max=a_max, b_min=0.0, b_max=1.0, clip=True), 
+            CropForegroundd(keys=["vol", "seg"], source_key="vol"),
+            Resized(keys=["vol", "seg"], spatial_size=spatial_size),   
+            ToTensord(keys=["vol", "seg"]),
+
+        ]
+    )
+
+    test_transforms = Compose(
+        [
+            LoadImaged(keys=["vol", "seg"]),
+            AddChanneld(keys=["vol", "seg"]),
+            Spacingd(keys=["vol", "seg"], pixdim=pixdim, mode=("bilinear", "nearest")),
+            Orientationd(keys=["vol", "seg"], axcodes="RAS"),
+            ScaleIntensityRanged(keys=["vol"], a_min=a_min, a_max=a_max,b_min=0.0, b_max=1.0, clip=True), 
+            CropForegroundd(keys=['vol', 'seg'], source_key='vol'),
+            Resized(keys=["vol", "seg"], spatial_size=spatial_size),   
+            ToTensord(keys=["vol", "seg"]),
+
+            
+        ]
+    )
+
+    if cache:
+        train_ds = CacheDataset(data=train_files, transform=train_transforms,cache_rate=1.0)
+        train_loader = DataLoader(train_ds, batch_size=1)
+
+        test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0)
+        test_loader = DataLoader(test_ds, batch_size=1)
+
+        return train_loader, test_loader
+
+    else:
+        train_ds = Dataset(data=train_files, transform=train_transforms)
+        train_loader = DataLoader(train_ds, batch_size=1)
+
+        test_ds = Dataset(data=test_files, transform=test_transforms)
+        test_loader = DataLoader(test_ds, batch_size=1)
+
+        return train_loader, test_loader
