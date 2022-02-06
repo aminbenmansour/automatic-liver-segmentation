8f80df4edd00564762563a6350d9dab5e0cfe004
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-06 02:02:51 +0100

    patient visiualization

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import dicom2nifti
 import numpy as np
 import nibabel as nib
+import matplotlib.pyplot as plt
 from monai.transforms import(
     Compose,
     AddChanneld,
@@ -93,3 +94,44 @@ def prepare(in_dir, pixdim=(1.5, 1.5, 1.0), a_min=-200, a_max=200, spatial_size=
         test_loader = DataLoader(test_ds, batch_size=1)
 
         return train_loader, test_loader    
+
+
+def show_patient(data, SLICE_NUMBER=1, train=True, test=False):
+    """
+    This function is to show one patient from your datasets, so that you can si if the it is okay or you need 
+    to change/delete something.
+    `data`: this parameter should take the patients from the data loader, which means you need to can the function
+    prepare first and apply the transforms that you want after that pass it to this function so that you visualize 
+    the patient with the transforms that you want.
+    `SLICE_NUMBER`: this parameter will take the slice number that you want to display/show
+    `train`: this parameter is to say that you want to display a patient from the training data (by default it is true)
+    `test`: this parameter is to say that you want to display a patient from the testing patients.
+    """
+
+    check_patient_train, check_patient_test = data
+
+    view_train_patient = first(check_patient_train)
+    view_test_patient = first(check_patient_test)
+
+    
+    if train:
+        plt.figure("Visualization Train", (12, 6))
+        plt.subplot(1, 2, 1)
+        plt.title(f"vol {SLICE_NUMBER}")
+        plt.imshow(view_train_patient["vol"][0, 0, :, :, SLICE_NUMBER], cmap="gray")
+
+        plt.subplot(1, 2, 2)
+        plt.title(f"seg {SLICE_NUMBER}")
+        plt.imshow(view_train_patient["seg"][0, 0, :, :, SLICE_NUMBER])
+        plt.show()
+    
+    if test:
+        plt.figure("Visualization Test", (12, 6))
+        plt.subplot(1, 2, 1)
+        plt.title(f"vol {SLICE_NUMBER}")
+        plt.imshow(view_test_patient["vol"][0, 0, :, :, SLICE_NUMBER], cmap="gray")
+
+        plt.subplot(1, 2, 2)
+        plt.title(f"seg {SLICE_NUMBER}")
+        plt.imshow(view_test_patient["seg"][0, 0, :, :, SLICE_NUMBER])
+        plt.show()
